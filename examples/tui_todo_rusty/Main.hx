58f81618a80acdad0e028df1b4c03778b8c6254f
Marcelo Serpa <boss@fullofcaffeine.com>
2026-01-25 01:12:05 -0600

    feat(examples): add rusty tui todo

---
+++ examples/tui_todo_rusty/Main.hx
@@ -0,0 +1,55 @@
+import rust.Vec;
+import rust.VecTools;
+import rust.tui.Action;
+import rust.tui.Tui;
+import Harness;
+
+class Main {
+	static function main(): Void {
+		var tasks = new Vec<Task>();
+		tasks.push(new Task("bootstrap reflaxe.rust", true));
+		tasks.push(new Task("add enums + switch", true));
+		tasks.push(new Task("ship ratatui demo", false));
+		tasks.push(new Task("ship rusty profile", false));
+
+		var selected = 0;
+		var running = true;
+
+		#if tui_headless
+		Tui.setHeadless(true);
+		#else
+		Tui.setHeadless(false);
+		#end
+
+		Tui.enter();
+		while (running) {
+			var a = VecTools.toArray(tasks.clone());
+			var lines = "";
+
+			var i = 0;
+			while (i < a.length) {
+				lines = lines + a[i].line(i == selected) + "\n";
+				i = i + 1;
+			}
+
+			Tui.render(lines);
+
+			var action = Tui.poll(250);
+			if (action == None) continue;
+
+			switch (action) {
+				case Up:
+					if (selected > 0) selected = selected - 1;
+				case Down:
+					if (selected < a.length - 1) selected = selected + 1;
+				case Toggle:
+					a[selected].toggle();
+				case Quit:
+					running = false;
+				case None:
+					// handled above
+			}
+		}
+		Tui.exit();
+	}
+}
